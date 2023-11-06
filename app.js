289e253c6aee747383b65de20ab2dd6f4d78034d
RoRo96PL <150021719+RoRo96PL@users.noreply.github.com>
2023-11-06 23:09:18 +0100

    app.js

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const THUMBNAILS = document.querySelectorAll(".thumbnail img");
-const POPUP = document.querySelector(".popup");
-const POPUP_CLOSE = document.querySelector(".popup__close");
-const POPUP_IMG = document.querySelector(".popup__img");
+document.addEventListener('DOMContentLoaded', function() {
+    const THUMBNAILS = document.querySelectorAll(".thumbnail img");
+    const POPUP = document.querySelector(".popup");
+    const POPUP_CLOSE = document.querySelector(".popup__close");
+    const POPUP_IMG = document.querySelector(".popup__img");
 
-THUMBNAILS.forEach((thumbnail) => {
-    thumbnail.addEventListener("click", (e) => {
-        POPUP.classList.remove("hidden");
-        POPUP_IMG.src = e.target.src;
+    const preloadImages = (src) => {
+        const img = new Image();
+        img.src = src;
+    };
+
+    THUMBNAILS.forEach((thumbnail) => {
+        thumbnail.addEventListener("click", (e) => {
+            const src = e.target.src;
+            preloadImages(src); // Wywołanie funkcji preładowania
+            POPUP.classList.remove("hidden");
+            POPUP_IMG.src = src;
+        });
     });
-});
 
-POPUP_CLOSE.addEventListener("click", () => {
-    POPUP.classList.add("hidden");
+    POPUP_CLOSE.addEventListener("click", () => {
+        POPUP.classList.add("hidden");
+    });
 });
