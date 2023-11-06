da568dbd83805344510f1d14d15b114c601df39e
RoRo96PL <150021719+RoRo96PL@users.noreply.github.com>
2023-11-06 14:52:40 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const THUMBNAILS = document.querySelectorAll(".thumbnail img");
+const POPUP = document.querySelector(".popup");
+const POPUP_CLOSE = document.querySelector(".popup__close");
+const POPUP_IMG = document.querySelector(".popup__img");
+
+THUMBNAILS.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", (e) => {
+        POPUP.classList.remove("hidden");
+        POPUP_IMG.src = e.target.src;
+    });
+});
+
+POPUP_CLOSE.addEventListener("click", () => {
+    POPUP.classList.add("hidden");
+});
